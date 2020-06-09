52530354009ea95af8ff6e847822656b112d76fd
Stanley Masinde <stanleyloren@gmail.com>
2020-06-09 10:03:25 +0300

    I have added tests * Intsalled mocha, mocha-http and chai * Set up a simple test * currently on the landing page is being tested because the API would require environment variables to be avialable in GH actions

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+
+chai.use(chaiHttp)
+chai.should()
+
+describe('Landing Page', () => {
+    it('Shows the landing page', (done) => {
+        chai.request(app)
+            .get('/').end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                res.should.have.status(200)
+                res.should.have.header('content-type', 'text/html; charset=UTF-8')
+                res.body.should.be.a('object')
+            })
+        done()
+    })
+})
+
+describe('API Calls', () => [
+    // TODO add API test functionality
+    it('This passes for now pending proper config', (done) => {
+        done()
+    })
+])
